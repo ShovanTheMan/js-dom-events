e2849b25e8a66064daedd2094ff94071c6331d05
ShovanTheMan <shovan2192@gmail.com>
2023-11-09 01:02:29 +0600

    eventlistener added

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 // option 1---directly set on the HTML element
-// option 2---add onclick option
+// option 2---add onclick option on the HTML document-----we use this------
+// <button onclick="makeRed()">Make Red</button>
 function makeRed(){
     document.body.style.backgroundColor= 'red';
 }
@@ -21,3 +22,25 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4
+
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click',makePink);
+
+function makePink(){
+ document.body.style.backgroundColor = 'pink'
+}
+
+// option 4 another
+
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
+
+//option 4 final-----important
+  //document.getElementById('make-goldenrod').addEventListener('click',function(){}
+
+document.getElementById('make-goldenrod').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenrod';} )
